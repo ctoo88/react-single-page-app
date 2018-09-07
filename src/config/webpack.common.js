9721c467703e842f165d2c77c8afe47f9068d32b
xu <13958055597@163.com>
2018-09-07 12:31:38 +0800

    react

--- src/config/webpack.common.js
+++ src/config/webpack.common.js
@@ -7,6 +7,14 @@ module.exports = {
     filename: 'app.js',
     path: path.resolve(__dirname, '../../www'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(sc|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../index.tpl.html'),
