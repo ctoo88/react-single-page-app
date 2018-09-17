fcd34af316a9d6fe37ce03f67165ed7d75edcdc8
xu <13958055597@163.com>
2018-09-17 11:23:32 +0800

    css-reset

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,7 +4,13 @@ import { Link } from 'react-router-dom';
 const About = () => (
   <div className="about animated slideInRight">
     <h2>About</h2>
-    back to <Link to="/">home</Link> page.
+    <p>back to <Link to="/">home</Link> page.</p>
+    
+    <div className="card">
+      <div className="card-content">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum laudantium possimus labore obcaecati et repellat, repellendus sapiente iure cumque aliquam quibusdam corporis, quo enim, asperiores, sint laboriosam commodi recusandae laborum.
+      </div>
+    </div>
   </div>
 );
 
