81513834fc14b9c29207fe02f024f13c69573eeb
ctoo88 <13958055597@163.com>
2018-11-16 03:13:58 +0000

    Update webpack.common.js

--- src/config/webpack.common.js
+++ src/config/webpack.common.js
@@ -7,10 +7,37 @@ module.exports = {
     filename: 'app.js',
     path: path.resolve(__dirname, '../../www'),
   },
+  resolve: {
+    // alias: {},
+    // modules: [../../src],
+    extensions: ['.js', '.jsx', '.scss'],
+  },
+  module: {
+    // noParse: [],
+    // rules: [],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../index.tpl.html'),
       hash: true,
     })
   ],
+  optimization: {
+    splitChunks: {
+      chunks: 'initial',
+      minSize: 30000,
+      cacheGroups: {
+        default: false,
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          reuseExistingChunk: true,
+        },
+      }
+    }
+  },
+  performance: {
+    maxEntrypointSize: 4000000,
+    maxAssetSize: 1000000,
+  }
 };
